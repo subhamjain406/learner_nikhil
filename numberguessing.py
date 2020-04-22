e857b20a4d87d93182096cbdfe0f93f7677f1a84
SUBHAM JAIN <51990606+subhamjain406@users.noreply.github.com>
2020-04-22 16:00:16 +0530

    Update numberguessing.py

--- numberguessing.py
+++ numberguessing.py
@@ -3,7 +3,7 @@ import random
 print('lets play a game')
 n = random.randint(1,99)
 guess=int(input('enter any number between 1 to 99' ))
-while n!= guess:
+while guess:
     print()
     if guess < n:
         print('the number is too low')
@@ -12,8 +12,6 @@ while n!= guess:
         print ('the number is too high')
         guess = int(input('enter any number between 1 to 99'))
     else:
-
-        break
-print('u gussed it correct')
+        print('u gussed it correct')
 
 
